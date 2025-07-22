b623ba1f638013e9e78d3c308eeff440b08a451e
amasha2k <204845192+amasha2k@users.noreply.github.com>
2025-07-22 11:53:10 +0530

    Initial commit

---
+++ src/main/java/org/example/pahanafrontend/PahanaFrontendApplication.java
@@ -0,0 +1,13 @@
+package org.example.pahanafrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahanaFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PahanaFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pahanafrontend/PahanaFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pahanafrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PahanaFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
