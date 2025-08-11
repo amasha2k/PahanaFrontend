725f680dc2a4051bb073523b1c0ada6f7e920ea3
Amasha <204845192+amasha2k@users.noreply.github.com>
2025-08-11 17:24:25 +0530

    Port issue fixed

--- src/main/resources/static/api.js
+++ src/main/resources/static/api.js
@@ -1,5 +1,5 @@
 // Central place for all API endpoints
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "http://localhost:8980";
 const API_AUTH_LOGIN = `${API_BASE_URL}/api/auth/login`;
 const API_AUTH_REGISTER = `${API_BASE_URL}/api/auth/register`;
 const API_CUSTOMERS = `${API_BASE_URL}/api/customers`;
