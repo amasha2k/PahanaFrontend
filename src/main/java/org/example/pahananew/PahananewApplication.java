03cecd9d8618221ceee856e712fcdd85ddac9efd
TRD <204845192+amasha2k@users.noreply.github.com>
2025-08-11 17:23:09 +0530

    Boot up Blocker Issue

---
+++ src/main/java/org/example/pahananew/PahananewApplication.java
@@ -0,0 +1,13 @@
+package org.example.pahananew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahananewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PahananewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pahananew/PahananewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pahananew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PahananewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
