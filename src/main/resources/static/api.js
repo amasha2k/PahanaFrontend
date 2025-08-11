da21b3336a62ee9dae102db8ae83bdcb1d3adc99
Amasha <204845192+amasha2k@users.noreply.github.com>
2025-08-11 17:25:57 +0530

    Issue Identified & fixed

--- src/main/resources/static/api.js
+++ src/main/resources/static/api.js
@@ -1,5 +1,13 @@
 // Central place for all API endpoints
 const API_BASE_URL = "http://localhost:8080";
+const API_AUTH_LOGIN = `${API_BASE_URL}/api/auth/login`;
+const API_AUTH_REGISTER = `${API_BASE_URL}/api/auth/register`;
+const API_CUSTOMERS = `${API_BASE_URL}/api/customers`;
+const API_ITEMS = `${API_BASE_URL}/api/items`;
+const API_ORDERS = `${API_BASE_URL}/api/orders`;
+const API_DASHBOARD_STATS = `${API_BASE_URL}/api/dashboard/stats`;
+const API_DASHBOARD_ACTIVITY = `${API_BASE_URL}/api/dashboard/activity`;
+const API_AUTH_LOGOUT = `${API_BASE_URL}/api/auth/logout`;
 
 
 const Image_uri = `https://images.unsplash.com/photo-1494790108377-be9c29b29330?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D`;
